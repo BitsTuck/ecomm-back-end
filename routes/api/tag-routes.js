6f90ca65ee4f91911a736ad708dcf063f43077f7
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-02 18:02:08 -0400

    tag routes first pass

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,73 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const allTags = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(allTags);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagId = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!tagId) {
+      res.status(404).json({message: 'No tag with this id'});
+      return;
+    }
+    res.status(200).json(tagId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    })
+    if(!updateTag) {
+      res.status(404).json({message: 'tag not updated'})
+    }
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    if(!deleteTag)
+ {
+  res.status(404).json({ message: 'No such tag id'})
+ }  
+  res.status(200).json ({ message: 'Tag deleted'})
+} catch (err) {
+  res.status(500).json(err);
+}
   // delete on tag by its `id` value
 });
 
