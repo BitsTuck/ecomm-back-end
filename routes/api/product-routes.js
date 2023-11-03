7fd8c4f15787208382caa2b2c5c4a94a4b93fe9d
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-11-02 20:25:04 -0400

    final-ish routes and models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,12 +33,12 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const newProd = await Product.create(req.body);
-    res.status(200).json(newProd)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+  // try {
+  //   const newProd = await Product.create(req.body);
+  //   res.json(newProd).sendStatus(200)
+  // } catch (err) {
+  //   res.json(err).status(400)
+  // }
 
   /* req.body should look like this...
     {
@@ -115,7 +115,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteProduct = await Product.destroy ({
+      where: {
+        id : req.params.id,
+      }
+    })
+    if(!deleteProduct) {
+      res.status(404).json({ message: 'No such product id'})
+    } 
+    res.status(200).json({ message: 'Product deleted'})
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
